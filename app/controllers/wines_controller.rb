11c1136ca8748968c49b6d11ebc8995ba6c06cbb
Braulio <b.oyarzun.barros@gmail.com>
2022-03-25 01:35:34 -0300

    join table nested form

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -13,11 +13,12 @@ class WinesController < ApplicationController
   # GET /wines/new
   def new
     @wine = Wine.new
-    @wine.wine_strains.build.build_strain
+    @wine.strains.count+1.times{ @wine.wine_strains.build.build_strain }
   end
 
   # GET /wines/1/edit
   def edit
+    @wine.strains.count+1.times{ @wine.wine_strains.build.build_strain }
   end
 
   # POST /wines or /wines.json
@@ -66,6 +67,6 @@ class WinesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def wine_params
-      params.require(:wine).permit(:name, wine_strains_attributes: [:percentage, strain_attributes: [:name]])
+      params.require(:wine).permit(:name, wine_strains_attributes: [:percentage, :strain_id])
     end
 end
--- app/models/wine_strain.rb
+++ app/models/wine_strain.rb
@@ -1,6 +1,4 @@
 class WineStrain < ApplicationRecord
   belongs_to :wine
   belongs_to :strain
-
-  accepts_nested_attributes_for :strain
 end
