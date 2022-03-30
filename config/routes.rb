48df358ab387b8df26078559e40e47e6e2221bbf
Braulio <b.oyarzun.barros@gmail.com>
2022-03-30 20:53:01 -0300

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'wines#index'
+  
   resources :strains
   get '/wines/:id/add_new', to: 'wines#add_new', as: 'add_new'
   resources :wines
 
-  root 'wines#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
